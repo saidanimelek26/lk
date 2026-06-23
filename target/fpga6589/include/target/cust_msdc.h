2bf742f03e87c6986abc29501e08de5dc99a829f
melek saidani <mbcmelek3@gmail.com>
2026-06-23 06:15:24 -0700

    Update cust_msdc.h

--- target/fpga6589/include/target/cust_msdc.h
+++ target/fpga6589/include/target/cust_msdc.h
@@ -1,3 +1,38 @@
+/*****************************************************************************
+*  Copyright Statement:
+*  --------------------
+*  This software is protected by Copyright and the information contained
+*  herein is confidential. The software may not be copied and the information
+*  contained herein may not be used or disclosed except with the written
+*  permission of MediaTek Inc. (C) 2010
+*
+*  BY OPENING THIS FILE, BUYER HEREBY UNEQUIVOCALLY ACKNOWLEDGES AND AGREES
+*  THAT THE SOFTWARE/FIRMWARE AND ITS DOCUMENTATIONS ("MEDIATEK SOFTWARE")
+*  RECEIVED FROM MEDIATEK AND/OR ITS REPRESENTATIVES ARE PROVIDED TO BUYER ON
+*  AN "AS-IS" BASIS ONLY. MEDIATEK EXPRESSLY DISCLAIMS ANY AND ALL WARRANTIES,
+*  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF
+*  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NONINFRINGEMENT.
+*  NEITHER DOES MEDIATEK PROVIDE ANY WARRANTY WHATSOEVER WITH RESPECT TO THE
+*  SOFTWARE OF ANY THIRD PARTY WHICH MAY BE USED BY, INCORPORATED IN, OR
+*  SUPPLIED WITH THE MEDIATEK SOFTWARE, AND BUYER AGREES TO LOOK ONLY TO SUCH
+*  THIRD PARTY FOR ANY WARRANTY CLAIM RELATING THERETO. MEDIATEK SHALL ALSO
+*  NOT BE RESPONSIBLE FOR ANY MEDIATEK SOFTWARE RELEASES MADE TO BUYER'S
+*  SPECIFICATION OR TO CONFORM TO A PARTICULAR STANDARD OR OPEN FORUM.
+*
+*  BUYER'S SOLE AND EXCLUSIVE REMEDY AND MEDIATEK'S ENTIRE AND CUMULATIVE
+*  LIABILITY WITH RESPECT TO THE MEDIATEK SOFTWARE RELEASED HEREUNDER WILL BE,
+*  AT MEDIATEK'S OPTION, TO REVISE OR REPLACE THE MEDIATEK SOFTWARE AT ISSUE,
+*  OR REFUND ANY SOFTWARE LICENSE FEES OR SERVICE CHARGE PAID BY BUYER TO
+*  MEDIATEK FOR SUCH MEDIATEK SOFTWARE AT ISSUE.
+*
+*  THE TRANSACTION CONTEMPLATED HEREUNDER SHALL BE CONSTRUED IN ACCORDANCE
+*  WITH THE LAWS OF THE STATE OF CALIFORNIA, USA, EXCLUDING ITS CONFLICT OF
+*  LAWS PRINCIPLES.  ANY DISPUTES, CONTROVERSIES OR CLAIMS ARISING THEREOF AND
+*  RELATED THERETO SHALL BE SETTLED BY ARBITRATION IN SAN FRANCISCO, CA, UNDER
+*  THE RULES OF THE INTERNATIONAL CHAMBER OF COMMERCE (ICC).
+*
+*****************************************************************************/
+
 #ifndef CUST_MSDC_H
 #define CUST_MSDC_H
 
@@ -11,15 +46,26 @@
 #define MSDC_HIGHSPEED      (1 << 7)  /* high-speed mode support       */
 #define MSDC_UHS1           (1 << 8)  /* uhs-1 mode support            */
 #define MSDC_DDR            (1 << 9)  /* ddr mode support              */
+#define MSDC_HS200          (1 << 10) /* hs200 mode support            */
 
 #define MSDC_SMPL_RISING        (0)
 #define MSDC_SMPL_FALLING       (1)
 
-/* Add missing definitions */
+/* Clock source definitions */
 #define MSDC0_CLKSRC_DEFAULT    0
+#define MSDC1_CLKSRC_DEFAULT    0
+#define MSDC2_CLKSRC_DEFAULT    0
+
+/* Driver strength definitions */
 #define MSDC_DRVN_GEAR1         1
+#define MSDC_DRVN_GEAR2         2
+#define MSDC_DRVN_GEAR3         3
 #define MSDC_DRVN_DONT_CARE     0
-#define MSDC_MAX_NUM            1
+
+/* Maximum number of MSDC controllers */
+#ifndef MSDC_MAX_NUM
+#define MSDC_MAX_NUM 1
+#endif
 
 typedef enum {
     MSDC_CLKSRC_200MHZ = 0
@@ -37,17 +83,7 @@ struct msdc_cust {
     unsigned int   flags;             /* hardware capability flags     */
 };
 
-/* Define msdc_cap directly in the header */
-struct msdc_cust msdc_cap = {
-    .clk_src      = MSDC0_CLKSRC_DEFAULT,
-    .cmd_edge     = MSDC_SMPL_RISING,
-    .data_edge    = MSDC_SMPL_RISING,
-    .clk_drv      = MSDC_DRVN_GEAR1,
-    .cmd_drv      = MSDC_DRVN_GEAR1,
-    .dat_drv      = MSDC_DRVN_GEAR1,
-    .data_pins    = 8,
-    .data_offset  = 0,
-    .flags        = MSDC_CD_PIN_EN | MSDC_REMOVABLE,
-};
+/* Declaration - defined in cust_msdc.c */
+extern struct msdc_cust msdc_cap[MSDC_MAX_NUM];
 
 #endif /* end of _MSDC_CUST_H_ */
